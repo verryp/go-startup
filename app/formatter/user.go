d91bfe008b6d42f1049dcc0e69614d9add382f9a
verryp <verryp.dev@gmail.com>
2021-08-01 23:45:32 +0700

    chore: adding user format for json response

---
+++ app/formatter/user.go
@@ -0,0 +1,23 @@
+package formatter
+
+import "github.com/verryp/go-startup/app/model"
+
+type UserFormatter struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Occupation string `json:"occupation"`
+	Email string `json:"email"`
+	Token string `json:"token"`
+}
+
+func FormatUser(user model.User, token string) UserFormatter {
+	formatter := UserFormatter{
+		ID: user.ID,
+		Name: user.Name,
+		Occupation: user.Occupation,
+		Email: user.Email,
+		Token: token,
+	}
+
+	return formatter
+}
